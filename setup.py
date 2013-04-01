18b71a1f1bfceaf1d6249470a7c564c76a6120dc
piotr olejarz <vadwook@hotmail.com>
2013-04-01 16:14:20 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+
+setup(
+    name='CsvSchema',
+    version='1.0.0',
+    author='Piotr Olejarz',
+    author_email='vadwook@hotmail.com',
+    url='http://bitbucket.org/vadwook/csvschema/',
+    packages=['csv_schema', 'csv_schema.columns', 'csv_schema.structure'],
+    license='LICENSE.txt',
+    description='Tool for describing CSV data structure.',
+    long_description=open('README.rst').read(),
+)
