4a4974018ad6ce02b1ba7846f57b47c18a55edff
piotr olejarz <vadwook@hotmail.com>
2013-04-20 21:25:07 +0200

    Added changes needed for PyPI upload

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
 from distutils.core import setup
 
 
+long_description = ''
+try:
+    long_description = open('README.rst', 'rt').read()
+except IOError:
+    pass
+
+
 setup(
     name='CsvSchema',
     version='1.1.0',
@@ -9,6 +16,15 @@ setup(
     url='http://bitbucket.org/vadwook/csvschema/',
     packages=['csv_schema', 'csv_schema.columns', 'csv_schema.structure'],
     license='LICENSE.txt',
-    description='Tool for describing CSV data structure.',
-    long_description=open('README.rst').read(),
+    description='Module for describing CSV data structure.',
+    long_description=long_description,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development',
+        'Topic :: Text Processing',
+    ],
 )
